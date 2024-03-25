268909478d267da5108770ef0c943230e47af33b
alkaisha02 <alkaisha02@gmail.com>
2024-03-25 19:21:04 +0500

    changes made

--- event.py
+++
@@ -1,7 +0,0 @@
-# event.py
-class Event:
-    def __init__(self, name, date, location, ticket_price):
-        self.name = name
-        self.date = date
-        self.location = location
-        self.ticket_price = ticket_price
